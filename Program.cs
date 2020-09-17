1fcd254b75c5ff2360af39d08b67a0741546e115
Emad Alashi <emad.ashi@gmail.com>
2020-09-17 21:21:00 +1000

    Connect to twitch channel and display print contributors

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using TwitchLib.Client;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+using TwitchLib.Communication.Clients;
+using TwitchLib.Communication.Models;
+
+namespace ThankYou
+{
+    class Program
+    {
+        private static List<string> _contributorsToday = new List<string>();
+
+        //TODO: Take an argument to establish git repo
+        //TODO: from an environment variable capture the channel ULR and token
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //TODO: Connect to the Twitch channel
+            var twitchUserName = "emadashi"; //TODO: don't forget to parameterise
+            var accessToken = ""; //TODO: don't forget to parameterise
+            var channelName = "emadashi"; //TODO: don't forget to parameterise
+
+
+            ConnectionCredentials credentials = new ConnectionCredentials(twitchUserName, accessToken);
+            var clientOptions = new ClientOptions
+            {
+                MessagesAllowedInPeriod = 750,
+                ThrottlingPeriod = TimeSpan.FromSeconds(30),
+            };
+
+            WebSocketClient customClient = new WebSocketClient(clientOptions);
+            var client = new TwitchClient(customClient);
+            client.Initialize(credentials, channelName);
+            client.OnMessageReceived += Client_OnMessageReceived;
+            client.OnLog += Client_OnLog;
+            client.Connect();
+
+            Console.ReadKey(true);
+
+            foreach( var contributor in _contributorsToday) 
+            {
+                Console.WriteLine(contributor);
+            }
+        }
+
+        private static void Client_OnLog(object sender, OnLogArgs e)
+        {
+            Console.WriteLine($"Log {e.DateTime}: botname: {e.BotUsername}, Data: {e.Data}");
+        }
+
+        private static void Client_OnMessageReceived(object sender, OnMessageReceivedArgs e)
+        {
+            var input = e.ChatMessage.Message;
+            var author = e.ChatMessage.Username;
+
+            var messageTokens = e.ChatMessage.Message.Split(' ');
+            if( messageTokens.Length > 1 && messageTokens[0] == "!thanks")
+            {
+                if (messageTokens.Length > 2)
+                {
+                    //TODO: respond to the channel and say there should be only one argument to !thanks
+                }
+                _contributorsToday.Add(messageTokens[1]);
+            }
+            Console.WriteLine($"{author} wrote this: {input}");
+        }
+    }
+}
