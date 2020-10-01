a0bec11ec576677325d8c23351c356d8bf07190b
Emad Alashi <emad.ashi@gmail.com>
2020-10-01 21:37:05 +1000

    Parse readme.md and identify Contributors segment

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using CommandLine;
 using LibGit2Sharp;
+using Microsoft.Toolkit.Parsers.Markdown;
+using Microsoft.Toolkit.Parsers.Markdown.Blocks;
 using TwitchLib.Client;
 using TwitchLib.Client.Events;
 using TwitchLib.Client.Models;
@@ -62,12 +64,47 @@ namespace ThankYou
         {
             //TODO: please move to be taken from a commandline from the chat stream
             var repoUrl = "https://github.com/eashi/thankyou";
+            var contributorsHeader = "Acknowledgement"; //TODO: this should be a configuration
+            var fileHoldingContributorsInfo = "readme.md"; //TODO: this should be a configuration
 
-            var co = new CloneOptions();
-            co.CredentialsProvider = (_url, _user, _cred) => new UsernamePasswordCredentials { Username = username, Password = password };
+            var cloneOptions = new CloneOptions();
+            cloneOptions.CredentialsProvider = (_url, _user, _cred) => new UsernamePasswordCredentials { Username = username, Password = password };
             var tempPath = Path.GetTempPath();
-            var directoryInfo = Directory.CreateDirectory(Path.Combine(tempPath, "Jaan"));
-            Repository.Clone(repoUrl, directoryInfo.FullName, co);
+            var tempPathGitFolder = Path.Combine(tempPath, "Jaan"); //TODO: remove "Jaan" and create random folder?
+            if(!Directory.Exists(tempPathGitFolder))
+            {
+                var directoryInfo = Directory.CreateDirectory(tempPathGitFolder);
+            }
+            Repository.Clone(repoUrl, tempPathGitFolder, cloneOptions); //TODO: if the repo clone is already here, should we delete and reclone? or should we assume correct repo here.
+
+            var pathToReadme = Path.Combine(tempPathGitFolder, fileHoldingContributorsInfo);
+            var readMeContent = File.ReadAllText(pathToReadme);
+
+            var readMeFileParsed = new MarkdownDocument();
+            readMeFileParsed.Parse(readMeContent);
+
+            //find the "CONTRIBUTORS" section. The title of the section should be a configuration
+            for(int index = 0; index <= readMeFileParsed.Blocks.Count-1;  index++)
+            {
+                Console.WriteLine(readMeFileParsed.Blocks[index].ToString());
+                if(readMeFileParsed.Blocks[index] is HeaderBlock &&  readMeFileParsed.Blocks[index].ToString().Trim() == contributorsHeader)
+                {
+                    for(int index2=index+1; index2 <= readMeFileParsed.Blocks.Count-1; index2++)
+                    {
+                        if (readMeFileParsed.Blocks[index2] is ListBlock)
+                        {
+                            Console.WriteLine($"Here is your friends: {readMeFileParsed.Blocks[index2].ToString()}");
+                            break;
+                        }
+                    }
+                }
+            }
+
+            //TOOD: change the file and save it
+
+            //TODO: commit the file to the repo, on a non-master branch
+
+            //TODO: push the commit to origin
         }
 
         private static void Client_OnLog(object sender, OnLogArgs e)
